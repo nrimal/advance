8be4b32d129d725f5b3af79fe0efaa353e7244d4
nrimal <rimal.5@osu.edu>
2018-02-19 19:10:13 -0500

    "stuff"

--- advance/app/controllers/survey_controller.rb
+++ advance/app/controllers/survey_controller.rb
@@ -1,13 +1,18 @@
 class SurveyController < ApplicationController
-  def show
+  
+  
+  
+  def suggestion
+    @suggestions = [""]
+
   end
 
   def result
-
+    
     @sucidalDepress= params[:Question2] + params[:Question6] + params[:Question9]
     @foodSeverity = params[:Question5] 
     @general = params[:Question1] + params[:Question3] + params[:Question4] + params[:Question7] + params[:Question8]
-    
+
     @general = @general.reduce(0) {|sum, element|
       sum += element.to_i
     }
@@ -18,8 +23,8 @@ class SurveyController < ApplicationController
       sum += element.to_i
     }
     @severity = (params[:Question10])[0].to_i
-
   end
+
 
 
 end
