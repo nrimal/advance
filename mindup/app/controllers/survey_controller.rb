66ed9d839a2637d155f932abe720f0b0b6bde2ef
Jack Dubbs <dubbs.14@osu.edu>
2018-03-23 16:58:23 -0400

    Added anxiety_survey and the corresponding views

--- mindup/app/controllers/survey_controller.rb
+++ mindup/app/controllers/survey_controller.rb
@@ -1,16 +1,17 @@
 class SurveyController < ApplicationController
-  
-  
-  
+
+  def show
+  end
+
   def suggestion
     @suggestions = [""]
 
   end
 
   def result
-    
+
     @sucidalDepress= params[:Question2] + params[:Question6] + params[:Question9]
-    @foodSeverity = params[:Question5] 
+    @foodSeverity = params[:Question5]
     @general = params[:Question1] + params[:Question3] + params[:Question4] + params[:Question7] + params[:Question8]
 
     @general = @general.reduce(0) {|sum, element|
--- mindup/config/routes.rb
+++ mindup/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get '/survey/show' => 'survey#show'
+  get '/survey/result' => 'survey#result'
+  get '/anxiety_survey/show' => 'anxiety_survey#show'
+  get '/anxiety_survey/result' => 'anxiety_survey#result'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
