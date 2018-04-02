0c470c146c681db598ea5be0ea53ace1e2efd82c
Jack Dubbs <dubbs.14@osu.edu>
2018-04-02 18:02:16 -0400

    Edited the routes to set a defualt route

--- mindup/config/routes.rb
+++ mindup/config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   get '/test_home/index' => 'test_home#index'
   get '/test_home/survey/show' => 'survey#show'
   get '/test_home/anxiety_survey/show' => 'anxiety_survey#show'
+  #this is the default root command so localhost:3000 will go to test_home's index action
+  root 'test_home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
