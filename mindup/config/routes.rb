433565ef5587e27154738453db4e6a2be9f0a46e
code4community <rimal.5@osu.edu>
2018-04-09 18:42:48 -0400

    stuff

--- mindup/config/routes.rb
+++ mindup/config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
+
   devise_for :users
   get 'home/show'
 
   root to: 'home#show'
-=======
+
   get '/survey/show' => 'survey#show'
   get '/survey/result' => 'survey#result'
   get '/anxiety_survey/result' => 'anxiety_survey#result'
@@ -15,6 +15,5 @@ Rails.application.routes.draw do
 
   #this is the default root command so localhost:3000 will go to test_home's index action
   root 'test_home#index'
->>>>>>> b106575f372b36a0212c715cf4dba8d975ac6d15
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
