15d822adaee0b43310a60fe517421037ff72b41a
Jack Dubbs <dubbs.14@osu.edu>
2018-10-08 20:13:27 -0400

    updated the algorithm

--- app/controllers/anxiety_survey_controller.rb
+++ app/controllers/anxiety_survey_controller.rb
@@ -2,151 +2,33 @@ class AnxietySurveyController < ApplicationController
   def show
   end
 
-# object - has a catagory property. Catagory has a weight property. Catagory has a id.
-# Catagory has options(onject). Options - array of weights. Option
-#catagory:{
-#  catagoryid:4,
-#  weight:1
-#}
-#options:[ {weight: 2},{weight: 3},{weight: 4} ... ]
-class Question
-  def initialize(id,text,catagories,weights)
-    #question id
-    @id = id
-    #the question itself
-    @text = text
-    #an array of catagories, 1for1 and ordered the same as weights
-    @catagories = catagories
-    #an array of weights, 1for1 and ordered the same as catagories
-    @weights = weights
-  end
-
-  #set function for id
-  def set_id (id)
-    @id = id
-  end
-  #set function for text
-  def set_text (text)
-    text
-  end
-  #set function for catagories
-  def set_catagories (catagories)
-    catagories
-  end
-  #set function for weights
-  def set_weights (weights)
-    weights
-  end
-
-  #get function for id
-  def get_id
-    @id
-  end
-  #get function for text
-  def get_text
-    @text
-  end
-  #get function for catagories
-  def get_catagories
-    @catagories
-  end
-  #get function for weights
-  def get_weights
-    @weights
-  end
-end
-
-class Options
-  def initialize(question_id,texts,weights)
-    #The integer associated with the question.
-    #Should be the same as the question's id.
-    @question_id = question_id
-    #An array of texts that is the different 'answers' to the question asked
-    @texts = texts
-    #An array of weights that corrospond to the texts, 1 for 1 with texts
-    @weights = weights
-  end
-  #Set function for question_id
-  def set_question_id (question_id)
-    @question_id = question_id
-  end
-  #set function for texts
-  def set_texts (texts)
-    @texts = texts
-  end
-  #set function for weights
-  def set_weights (weights)
-    @weights = weights
-  end
-  #get function for question_id
-  def get_question_id
-    @question_id
-  end
-  #get function for texts
-  def get_texts
-    @texts
-  end
-  #get function for weights
-  def get_weights
-    @weights
-  end
-end
-
-class Suggestion
-  def initialize(catagory, id, link)
-    @catagory = catagory
-    @id = id
-    @link = link
-  end
-  #set function for catagory
-  def set_catagory (catagory)
-    @catagory = catagory
-  end
-  #set function for id
-  def set_id
-    @id = id
-  end
-  #set function for link
-  def set_link (link)
-    @link = link
-  end
-  #get function for catagory
-  def get_catagory
-    @catagory
-  end
-  #get function for id
-  def get_id
-    @id
-  end
-  #get function for link
-  def get_link
-    @link
-  end
-end
-
-
   def result
-#Ask the options object which catagory it is identified with.
 
-#Once in the specific catagory, iterate over the options, asking for the weights
+#We will recieve a lot of objects
+#for each options
+  #Ask the options object what it's question_id is.
 
-#assign the weights to the catagory varible, doing calculations based on weight
-# of catagory / questions /
+  #With the question_id, we can figure out the catagories associated with that question
+  #With the options, we can figure out the user's coice for that question
+  #We can add together all of the catagories that show up.
+    #all their corresonding weights so that we can judge which are the most important ones
+    #all the user's choices for those questions and which catagory they belong to
+#we now have a big collection of catagories, with their weights from each question
+#we now have a big collection of user's responses that tell us the weight that they
+  #have with each catagory.
+        # I believe this will end up being a number like - total weight
+        # Total weight (for each question) will be:
+          #the sum of all the  total_weight of each catagory
+        # There should also be a total_weight for each: catagory weight X the weight of the user's answer
 
-#Do this ^ for all of the options (which should be the entire list of ?'s')
-
-#Normalize the catagories into a 1 - 10 scale
+#So based on our total_weights for each catagory. we can do some calculations to more easily comapre:
+  #Normalize the catagories' total_weight into a 1 - 10 scale
 
 #have a check for any catagory above a certain threshold to short circuit the
 # code and have a SOS thing displayed
 
-#distribute suggestions based on each catagory.
-
-#return a suggestion object with links
-
-
-
-
+#return a suggestion object with links based on the calculated data
+#We will create a return object that will include the links (array) to be displayed to user
 
 
 #old code
@@ -183,11 +65,5 @@ end
     if not (params[:Question8].nil?)
       @severity = (params[:Question8])[0].to_i
     end
-
   end
-
-
-
-
-
 end
